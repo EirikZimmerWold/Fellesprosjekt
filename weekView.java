1adc75ac3c4f327a4380ed7937841dc6c8e08686
haakoho <hakon_hoiland@hotmail.com>
2014-03-12 10:47:29 +0100

    add weekPanel

---
+++ weekView.java
@@ -0,0 +1,42 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class weekView {
+	public static void main(String[] args){
+		generateWeek();
+		
+	}
+	
+	
+	public static void generateWeek(){
+		JFrame frame = new JFrame("Week");
+		frame.setSize(700, 1500);
+		
+		weekdayPanel wkdPanel = new weekdayPanel("Mandag", "01.01.2014");
+		weekdayPanel wkdPanel2 = new weekdayPanel("Tirsdag", "02.01.2014");
+		weekdayPanel wkdPanel3 = new weekdayPanel("Onsdag", "03.01.2014");
+		weekdayPanel wkdPanel4 = new weekdayPanel("Torsdag", "04.01.2014");
+		weekdayPanel wkdPanel5 = new weekdayPanel("Fredag", "05.01.2014");
+		weekdayPanel wkdPanel6 = new weekdayPanel("Lørdag", "06.01.2014");
+		weekdayPanel wkdPanel7 = new weekdayPanel("Søndag", "07.01.2014");
+		
+		wkdPanel.setSize(100, 100);
+		
+
+		frame.setLayout(new FlowLayout());
+		frame.getContentPane().add(wkdPanel);
+		frame.getContentPane().add(wkdPanel2);
+		frame.getContentPane().add(wkdPanel3);
+		frame.getContentPane().add(wkdPanel4);
+		frame.getContentPane().add(wkdPanel5);
+		frame.getContentPane().add(wkdPanel6);
+		frame.getContentPane().add(wkdPanel7);
+		
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ weekdayPanel.java
@@ -0,0 +1,61 @@
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
+
+import java.awt.GridBagLayout;
+
+
+public class weekdayPanel extends JPanel{
+
+	public weekdayPanel(String day, String date){
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		JLabel dayName = new JLabel(day);
+		JLabel dayDate = new JLabel(date);
+		
+		
+		DefaultListModel<String> data = new DefaultListModel<String>();
+		
+		//Test
+		
+		for (int i = 0; i<100; i++){
+		data.addElement("10:00" + "    " + "Møte");
+		//Trenger metoder for å hente ut møteinfo
+		}
+		
+	
+		JList<String> appointments = new JList<String>(data);
+		JScrollPane scrollPane = new JScrollPane(appointments);
+		appointments.setFixedCellHeight(25);	
+		appointments.setVisibleRowCount(10);
+			
+		
+		add(dayName, gc);
+		gc.gridy=1;
+		add(dayDate, gc);
+		
+		
+		gc.gridy=2;
+		add(scrollPane, gc);
+		
+		
+		System.out.println(data.getSize());
+		
+		
+
+		
+	}		
+}
