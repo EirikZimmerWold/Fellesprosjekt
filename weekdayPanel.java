42e086c8437848cc780586f8d7b800f4570ae0ce
EirikZimmerWold <eirikzimmer@hotmail.com>
2014-03-18 15:11:30 +0100

    avtaler dukker opp i lsite

--- weekdayPanel.java
+++ weekdayPanel.java
@@ -29,11 +29,16 @@ public class weekdayPanel extends JPanel implements PropertyChangeListener{
 	private JPanel list;
 	private GridBagConstraints gbc;
 	private ProgramFrame frame;
+	private Database db;
+	private String Dag;
+	private String Maaned;
+	private String aar;
 
 	public weekdayPanel(String day, ProgramFrame frame){
 		setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
 		this.frame=frame;
+		db = frame.getDB();
 		
 		dayName = new JLabel(day);
 		dayDate = new JLabel(" ");
@@ -61,10 +66,84 @@ public class weekdayPanel extends JPanel implements PropertyChangeListener{
 
 	public void setDate(String date){
 		dayDate.setText(date);
+		String [] del = date.split("/");
+		Dag = del[0];
+		Maaned = del[1];
+		aar = del[2];
+		
+	}
+
+	private boolean maanedsjekk(String tekst, String dato){
+		if (tekst.equals("Januar") && dato.equals("1")){
+			return true;
+		}
+		else if (tekst.equals("Feburar") && dato.equals("2")){
+			return true;
+		}
+		else if (tekst.equals("Mars") && dato.equals("3")){
+			return true;
+		}
+		else if (tekst.equals("April") && dato.equals("4")){
+			return true;
+		}
+		else if (tekst.equals("Mai") && dato.equals("5")){
+			return true;
+		}
+		else if (tekst.equals("Juni") && dato.equals("6")){
+			return true;
+		}
+		else if (tekst.equals("Juli") && dato.equals("7")){
+			return true;
+		}
+		else if (tekst.equals("August") && dato.equals("8")){
+			return true;
+		}
+		else if (tekst.equals("September") && dato.equals("9")){
+			return true;
+		}
+		else if (tekst.equals("Oktober") && dato.equals("10")){
+			return true;
+		}
+		else if (tekst.equals("November") && dato.equals("11")){
+			return true;
+		}
+		else if (tekst.equals("Desember") && dato.equals("12")){
+			return true;
+		}
+		else{ return false;}
 	}
 	
-	public void leggeTilAvtale(Ansatt eier){
+	private void fjerneFraListe(){
+		list.removeAll();
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+        gc.weighty = 1;
+        gc.weighty= 1;
+        gc.gridx=0;
+        gc.gridy=2;
 		
+	}
+	public void leggeTilAvtale(Ansatt eier) throws SQLException{
+		fjerneFraListe();
+		String avtaler;
+		if (eier != null){
+			avtaler = db.avtalerPersonErMed(eier);
+			String [] delt = avtaler.split("-");
+			for (String verdi:delt){
+				if (verdi != ""){
+					int avID = Integer.parseInt(verdi);
+					Avtale avtalen=db.getBestemtAvtale(avID);
+					String [] avtaledag = avtalen.getStartTid().split("-");
+					if (Dag.equals(avtaledag[2])){
+						if(aar.equals(avtaledag[0])){
+							if (maanedsjekk(avtaledag[1],Maaned) == true){
+								addAvtalePanel(avtalen);
+							}	
+						}
+					}
+				}
+			}
+		}
 	}
 	public void addAvtalePanel(Avtale avtale){
 		AvtalePanel panel=new AvtalePanel(avtale);
